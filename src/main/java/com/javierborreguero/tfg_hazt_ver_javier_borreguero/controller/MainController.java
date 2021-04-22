1d1a38eac4c01454425daf6a573a2d82f30d0ef2
javierboreguero <javierborreguerodelpozo@gmail-com>
2021-04-22 09:25:27 +0200

    Creating Navigation with animated menu icon

---
+++ src/main/java/com/javierborreguero/tfg_hazt_ver_javier_borreguero/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.javierborreguero.tfg_hazt_ver_javier_borreguero.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Clase que se encarga de atender peticiones y derivarnos a la vista correspondiente
+ */
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String aboutMe() {
+        return "Main_Screen";
+    }
+    @GetMapping("/Projects")
+    public String MyProjects(){
+        return "My_Projects";
+    }
+}
--- src/main/java/com/javierborreguero/tfg_hazt_ver_javier_borreguero/repository/MyRepository.java
+++ src/main/java/com/javierborreguero/tfg_hazt_ver_javier_borreguero/repository/MyRepository.java
@@ -1,8 +1,8 @@
 package com.javierborreguero.tfg_hazt_ver_javier_borreguero.repository;
 
 import com.javierborreguero.tfg_hazt_ver_javier_borreguero.entity.MyEntity;
-import org.springframework.data.repository.CrudRepository;
 
-public interface MyRepository extends CrudRepository<MyEntity, Long> {
+
+public interface MyRepository {
 
 }
