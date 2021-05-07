9a8ef079d81c40e2829f348f081242677b003f89
javierboreguero <javierborreguerodelpozo@gmail-com>
2021-05-07 18:08:00 +0200

    Main_Screen done

--- src/main/java/com/javierborreguero/tfg_hazt_ver_javier_borreguero/configuration/MyConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.javierborreguero.tfg_hazt_ver_javier_borreguero.configuration;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class MyConfiguration {
-}
--- src/main/java/com/javierborreguero/tfg_hazt_ver_javier_borreguero/repository/MyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.javierborreguero.tfg_hazt_ver_javier_borreguero.repository;
-
-import com.javierborreguero.tfg_hazt_ver_javier_borreguero.entity.MyEntity;
-
-
-public interface MyRepository {
-
-}
