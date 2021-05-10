9e705431f022e55b9f90ceb91ad8302966957c35
javierboreguero <javierborreguerodelpozo@gmail-com>
2021-05-10 19:29:11 +0200

    Preparing to send message

---
+++ src/main/resources/static/php/SendEmail.php
@@ -0,0 +1,10 @@
+<!-- Cuando el usuario me envía un mensaje lo recibo en el gmail -->
+<?php
+$visitorName = $_POST['mame'];
+$visitorEmail = $_POST['email'];
+$subject = $_POST['subject'];
+$message = &$_POST['email'];
+$email_from = 'info@yourwebsite.com';
+?>
+
+
