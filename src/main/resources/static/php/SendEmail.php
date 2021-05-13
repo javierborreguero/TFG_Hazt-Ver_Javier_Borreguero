229cbf8cc9fd8d3160c4fbe87c2dede43cc337ca
javierboreguero <javierborreguerodelpozo@gmail-com>
2021-05-13 23:56:59 +0200

    Sendin message to email from Contact_Me.html
    - Next step, capture email address 
    - Finish CV and Projects

--- src/main/resources/static/php/SendEmail.php
+++ src/main/resources/static/php/SendEmail.php
@@ -1,10 +1,17 @@
 <!-- Cuando el usuario me envía un mensaje lo recibo en el gmail -->
 <?php
-$visitorName = $_POST['mame'];
-$visitorEmail = $_POST['email'];
+$visitor_name = $_POST['mame'];
+$visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = &$_POST['email'];
 $email_from = 'info@yourwebsite.com';
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $visitor_name.\n" . "User Email: $visitor_email.\n" . "Subject: $subject.\n" . "User Message: $message.\n";
+$to = 'javierborreguerodelpozo@gmail.com';
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+mail($to, $email_subject, $email_body, $headers);
+header("Location: /Contact");
 ?>
 
 
